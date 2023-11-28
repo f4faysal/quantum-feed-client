3657935027fd306f0d47047616459275e671ca66
f4faysal <f4faysals@gmail.com>
2023-11-29 00:20:40 +0600

    Add form label and link to sign-in page in ForgotPassword component

--- components/form/ForgotPasseord.tsx
+++ components/form/ForgotPasseord.tsx
@@ -11,11 +11,13 @@ import {
   FormControl,
   FormField,
   FormItem,
+  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useForgotPasswordMutation } from "@/redux/api/authApi";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
 
 const formSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
@@ -23,6 +25,7 @@ const formSchema = z.object({
 
 const ForgotPassword = () => {
   const [res, setRes] = useState<any>(null);
+  const [isSend, setIsSend] = useState<boolean>(false);
   const [forgotPassword] = useForgotPasswordMutation();
 
   const router = useRouter();
@@ -39,8 +42,8 @@ const ForgotPassword = () => {
       const res: any = await forgotPassword(values);
 
       if (res?.data?.message) {
-        router.push("/sign-in");
         form.reset();
+        setIsSend(true);
         toast.success("Check your email for reset password link!");
       } else {
         toast.error(res?.error);
@@ -53,14 +56,24 @@ const ForgotPassword = () => {
 
   return (
     <div className="h-screen w-full flex justify-center items-center">
-      <div>
+      <div className="border md:bg-white px-5 py-9 rounded-sm shadow-md w-80">
+        <h1 className="text-base font-bold">Forgot Password</h1>
+        {isSend && (
+          <p className="text-xs mt-3">
+            Check your email for reset password link! <br />
+            <Link href="/sign-in" className="text-blue-500 hover:underline">
+              Back to Login
+            </Link>
+          </p>
+        )}
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
+          <form className="mt-5" onSubmit={form.handleSubmit(onSubmit)}>
             <FormField
               control={form.control}
               name="email"
               render={({ field }) => (
                 <FormItem>
+                  <FormLabel>Email address</FormLabel>
                   <FormControl>
                     <Input {...field} placeholder="Email address" />
                   </FormControl>
@@ -69,12 +82,13 @@ const ForgotPassword = () => {
               )}
             />
 
-            <div className="text-left px-16">
+            <div className="text-right">
               <Button type="submit" className="  mt-2  font-semibold ">
                 Forgot Password
               </Button>
             </div>
           </form>
+
           <p className="text-red-600 text-sm">{res}</p>
         </Form>
       </div>
